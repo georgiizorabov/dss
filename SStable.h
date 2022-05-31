f64da44d8b44704c385eb03174faeb38ebde0759
georgiizorabov <georgiizorabov@yandex.ru>
2022-05-31 21:55:18 +0300

    second commit

---
+++ SStable.h
@@ -0,0 +1,17 @@
+
+#ifndef SHDPROJ_SSTABLE_H
+#define SHDPROJ_SSTABLE_H
+
+class SSTable {
+    void addLog();
+
+    void find();
+
+    void removeElement();
+
+//    SSTableFabric fabric;
+//    filter filter;
+//    sparseSSTable sparseSSTable;
+};
+
+#endif //SHDPROJ_SSTABLE_H
