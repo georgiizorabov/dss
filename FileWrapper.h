19c064fe2627d188aac07e45b58975c840440ec3
Fedor Kudriavtsev <koufesser@gmail.com>
2022-06-01 19:18:07 +0300

    FileWrapper

---
+++ FileWrapper.h
@@ -0,0 +1,14 @@
+//
+// Created by Fedya on 01.06.2022.
+//
+
+#ifndef SHDPROJ_FILEWRAPPER_H
+#define SHDPROJ_FILEWRAPPER_H
+#include "kvs.h"
+
+class FileWrapper {
+
+};
+
+
+#endif //SHDPROJ_FILEWRAPPER_H
--- SparseSSTable.h
+++ SparseSSTable.h
@@ -1,9 +1,31 @@
 #pragma once
 
+#include "kvs.h"
+#include <utility>
+#include <optional>
+#include "SStable.h"
 
 class SparseSSTable {
-    pair<int, int> getBounds
-            recount
+    char const *ch = "";
+    std::vector<KeyOffset> sparse = {};
+    std::pair<std::optional<KeyOffset>, std::optional<KeyOffset>> getBounds(Key key) {
+        std::optional<KeyOffset> start;
+        std::optional<KeyOffset> end;
+
+        for (int i = 0; i < sparse.size(); ++i) {
+            if (strcmp(sparse[i].getKey().getKey(), key.getKey()) <= 0)
+                start = sparse[i];
+            if (strcmp(sparse[i].getKey().getKey(), key.getKey()) >= 0) {
+                end = sparse[i];
+                break;
+            }
+        }
+
+        return {start, end};
+    }
+    void recount() {
+
+    }
     поля:
             array <pair<key, offset>>
 
