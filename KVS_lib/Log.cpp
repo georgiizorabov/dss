7364b2d223306b540e6e656ef6a7b7886f1489e4
georgiizorabov <georgiizorabov@yandex.ru>
2022-06-07 16:16:50 +0300

    filter2

--- KVS_lib/Log.cpp
+++ KVS_lib/Log.cpp
@@ -3,16 +3,16 @@
 #include "KVS.h"
 #include "Log.h"
 
-std::optional<KeyOffset> Log::find(const Key& key) {
-    for (const auto &el: log) {
-        if (el.getKey().getKey() == key.getKey()) {
-            return (KeyOffset(el.getKey(), el.getOffset(), el.deleted));
+std::optional<KeyOffset> Log::find(const Key &key) {
+    for (int i = log.size() - 1; i >= 0; i--) {
+        if (log[i].getKey().getKey() == key.getKey()) {
+            return (KeyOffset(log[i].getKey(), log[i].getOffset(), log[i].deleted));
         }
     }
     return std::nullopt;
 }
 
-void Log::remove(const Key& key) {
+void Log::remove(const Key &key) {
     for (auto i = log.begin(); i != log.end(); i++) {
         if (i->getKey().getKey() == key.getKey()) {
             log.erase(i);
@@ -25,7 +25,7 @@ void Log::clear() {
     log.clear();
 }
 
-bool Log::add(const KeyOffset& keyOffset) {
+bool Log::add(const KeyOffset &keyOffset) {
     if (is_full())
         return false;
     log.push_back(keyOffset);
